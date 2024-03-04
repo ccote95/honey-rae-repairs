00a68b2214b5cabb9a27384987cf3892e293a6df
Chad Cote <chadcote58@gmail.com>
2024-03-04 10:13:40 -0500

    created the claim and close buttons and their functionality

--- src/views/ApplicationViews.jsx
+++ src/views/ApplicationViews.jsx
@@ -31,7 +31,10 @@ export const ApplicationViews = () => {
           }
         >
           <Route index element={<Welcome />} />
-          <Route path="tickets" element={<TicketList />} />
+          <Route
+            path="tickets"
+            element={<TicketList currentUser={currentUser} />}
+          />
           <Route path="employees">
             <Route index element={<EmployeesList />} />
             <Route path=":employeeId" element={<EmployeeDetails />} />

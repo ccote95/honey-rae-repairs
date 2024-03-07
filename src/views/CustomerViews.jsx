530f8d9798c4186da30097db1ea6a3855508a7ad
Chad Cote <chadcote58@gmail.com>
2024-03-07 12:09:30 -0500

    added the TicketForm component

--- src/views/CustomerViews.jsx
+++ src/views/CustomerViews.jsx
@@ -2,6 +2,7 @@ import { Outlet, Route, Routes } from "react-router-dom";
 import { Welcome } from "../components/welcome/Welcome.jsx";
 import { CustomerNavBar } from "../components/nav/CustomerNavBar.jsx";
 import { TicketList } from "../components/tickets/TicketList.jsx";
+import { TicketForm } from "../components/forms/TicketForm.jsx";
 
 export const CustomerViews = ({ currentUser }) => {
   return (
@@ -18,7 +19,7 @@ export const CustomerViews = ({ currentUser }) => {
         <Route index element={<Welcome />} />
         <Route path="tickets">
           <Route index element={<TicketList currentUser={currentUser} />} />
-          <Route path="create" element={<>ticket form</>} />
+          <Route path="create" element={<TicketForm />} />
         </Route>
       </Route>
     </Routes>
